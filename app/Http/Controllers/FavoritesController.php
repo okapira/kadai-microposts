3289008b083490ff6cf3507c4ba141aa70f9a054
kousuke.tamogami <okasuke.0714@gmial.com>
2022-01-31 13:41:55 +0000

    FavoritesController

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FavotiteContlloer extends Controller
+class FavoritesController extends Controller
 {
    /**
      * ユーザをお気に入りするアクション。
