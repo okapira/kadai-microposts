2f34f644c962419c004951f664f5b94a798dbb2c
kousuke.tamogami <okasuke.0714@gmial.com>
2022-01-31 13:27:23 +0000

    FavoritesController

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -19,7 +19,7 @@
                             {{-- ユーザ詳細ページへのリンク --}}
                             <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
                             {{-- お気に入り一覧ページへのリンク --}}
-                            <li class="dropdown-item">{!! link_to_route('users.show', 'Favorite', ['user' => Auth::id()]) !!}</li>
+                            <li class="dropdown-item">{!! link_to_route('users.favorites', 'Favorite', ['user' => Auth::id()]) !!}</li>
                             <li class="dropdown-divider"></li>
                             {{-- ログアウトへのリンク --}}
                             <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
